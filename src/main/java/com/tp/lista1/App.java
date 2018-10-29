63e916667214afb5f6a4491f19c497b9c51b0361
krzysztof <Kryzysk09@gmail.com>
2018-10-29 17:23:41 +0100

    World Hello added

--- src/main/java/com/tp/lista1/App.java
+++ src/main/java/com/tp/lista1/App.java
@@ -10,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("World Hello!");
         Account admin = new Account( "admin", "admin");
         System.out.println("Enter Login:");
         Scanner s = new Scanner(System.in);
